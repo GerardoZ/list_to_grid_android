00575c9220540ae174a3c31efa5493b820f1d896
Gerardo Zarate <gerardozarate.p@gmail.com>
2017-10-02 09:36:41 -0500

    First commit

---
+++ app/src/main/java/com/codecube/switch_list_to_grid/MainActivity.java
@@ -0,0 +1,13 @@
+package com.codecube.switch_list_to_grid;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
